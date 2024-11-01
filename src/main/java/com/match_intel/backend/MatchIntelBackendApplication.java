2666994dce91f7a2f8327bf453045c744f6ecc8e
IBlask <95757169+IBlask@users.noreply.github.com>
2024-11-01 22:49:52 +0100

    Init commit

---
+++ src/main/java/com/match_intel/backend/MatchIntelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.match_intel.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchIntelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchIntelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/match_intel/backend/MatchIntelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.match_intel.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchIntelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
