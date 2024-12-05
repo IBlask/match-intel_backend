8fb260d6fa143ba74c228442488c4f1f82791073
IBlask <95757169+IBlask@users.noreply.github.com>
2024-12-05 22:02:49 +0100

    Modify email validator; Add test

--- src/main/java/com/match_intel/backend/auth/utils/EmailValidator.java
+++ src/main/java/com/match_intel/backend/auth/utils/EmailValidator.java
@@ -8,6 +8,10 @@ import java.util.regex.Pattern;
 public class EmailValidator {
 
     public boolean validate(String email) {
+        if (email == null || email.isBlank()) {
+            return false;
+        }
+
         String EMAIL_PATTERN = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
                 + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 
