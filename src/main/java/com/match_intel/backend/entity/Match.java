4018a92456144d6e784110a8732f2b9dd5890df2
IBlask <95757169+IBlask@users.noreply.github.com>
2025-08-18 20:09:57 +0200

    Add dummy entities into the DB

--- src/main/java/com/match_intel/backend/entity/Match.java
+++ src/main/java/com/match_intel/backend/entity/Match.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@Table(name = "matches")
 @Getter
 public class Match {
     @Id
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,15 @@
 INSERT INTO users(id, username, first_name, last_name, email, password, enabled)
     VALUES
-        ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'jDoe1', 'John', 'Doe', 'blaskovic.ivan1@gmail.com', '$2a$10$/o9PxTASyyvPxy5/6etBeeExHZYBUzkm/7ROcGurq0Z30wyadcAM.', 1),
-        ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'jDoe2', 'John', 'Doe', 'blaskovic.ivan1+2@gmail.com', '$2a$10$/o9PxTASyyvPxy5/6etBeeExHZYBUzkm/7ROcGurq0Z30wyadcAM.', 1);
+        ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'ivan01', 'Ivan', 'Ivić', 'blaskovic.ivan1@gmail.com', '$2a$10$/o9PxTASyyvPxy5/6etBeeExHZYBUzkm/7ROcGurq0Z30wyadcAM.', 1),
+        ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'mmarkic', 'Marko', 'Markić', 'blaskovic.ivan1+1@gmail.com', '$2a$10$/o9PxTASyyvPxy5/6etBeeExHZYBUzkm/7ROcGurq0Z30wyadcAM.', 1),
+        ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 'pere', 'Pero', 'Perić', 'blaskovic.ivan1+2@gmail.com', '$2a$10$/o9PxTASyyvPxy5/6etBeeExHZYBUzkm/7ROcGurq0Z30wyadcAM.', 1);
+
+INSERT INTO matches(id, player1_id, player2_id, initial_server, visibility, start_time, is_finished, final_score)
+    VALUES
+        ('906c02b7-471c-4aea-a5de-f75114e63fac', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'PUBLIC', '2025-06-16 05:33:51.688972', 1, '2 : 0'),
+        ('906c02b7-471c-4aea-a5de-f75114e63fa4', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'PUBLIC', '2025-06-17 05:33:51.688972', 1, '2 : 1'),
+        ('906c02b7-471c-4aea-a5de-f75114e63fad', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'PUBLIC', '2025-06-16 06:33:51.688972', 1, '0 : 2');
+
+INSERT INTO follow_request(id, followee_id, follower_id, status, timestamp)
+    VALUES
+        ('906c02b7-471c-4aea-a5de-e75114e63fac', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'ACCEPTED', '2025-06-16 05:33:51.688972');
