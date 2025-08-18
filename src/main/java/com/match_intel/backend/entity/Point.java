55217d69886c66fd3992d5bc84f1305d92459546
IBlask <95757169+IBlask@users.noreply.github.com>
2025-08-18 21:28:51 +0200

    Fix points counting - use absolute points

--- src/main/java/com/match_intel/backend/entity/Point.java
+++ src/main/java/com/match_intel/backend/entity/Point.java
@@ -41,10 +41,10 @@ public class Point {
     private int player2Games;
 
     @Column(nullable = false)
-    private int player1Points;
+    private String player1Points;
 
     @Column(nullable = false)
-    private int player2Points;
+    private String player2Points;
 
 
     public Point(UUID matchId, int scoringPlayerNumber, int playerToServe) {
@@ -58,12 +58,12 @@ public class Point {
         this.player2Games = 0;
 
         if (scoringPlayerNumber == 1) {
-            this.player1Points = 1;
-            this.player2Points = 0;
+            this.player1Points = "15";
+            this.player2Points = "0";
         }
         else {
-            this.player1Points = 0;
-            this.player2Points = 1;
+            this.player1Points = "0";
+            this.player2Points = "15";
         }
     }
 
@@ -80,26 +80,47 @@ public class Point {
         this.player1Points = parentPoint.getPlayer1Points();
         this.player2Points = parentPoint.getPlayer2Points();
 
-        if (scoringPlayerNumber == 1) {
-            this.player1Points++;
-        }
-        else {
-            this.player2Points++;
-        }
-
-        // if not tie-break
+        // If not tie-break
         if (!(this.player1Games == 6 && this.player2Games == 6)) {
-            if (this.player1Points >= 4 && (this.player2Points + 2) <= this.player1Points) {
+            // Add point to the scoring player
+            if (scoringPlayerNumber == 1) {
+                switch (this.player1Points) {
+                    case "0" -> this.player1Points = "15";
+                    case "15" -> this.player1Points = "30";
+                    case "30" -> this.player1Points = "40";
+                    case "40" -> this.player1Points = "Ad";
+                    case "Ad" -> this.player1Points = "GAME";
+                }
+            }
+            else {
+                switch (this.player2Points) {
+                    case "0" -> this.player2Points = "15";
+                    case "15" -> this.player2Points = "30";
+                    case "30" -> this.player2Points = "40";
+                    case "40" -> this.player2Points = "Ad";
+                    case "Ad" -> this.player2Points = "GAME";
+                }
+            }
+
+            // check if Game is won
+            if (this.player1Points.equals("Ad") && this.player2Points.equals("Ad")) {
+                this.player1Points = "40";
+                this.player2Points = "40";
+            }
+            else if (this.player1Points.equals("GAME")
+                    || (this.player1Points.equals("Ad") && !this.player2Points.equals("40"))) {
                 this.player1Games++;
                 this.resetPoints();
                 this.changePlayerToServe();
             }
-            else if (this.player2Points >= 4 && (this.player1Points + 2) <= this.player2Points) {
+            else if (this.player2Points.equals("GAME")
+                    || (this.player2Points.equals("Ad") && !this.player1Points.equals("40"))) {
                 this.player2Games++;
                 this.resetPoints();
                 this.changePlayerToServe();
             }
 
+            // check if Set is won
             if (this.player1Games >= 6 && (this.player2Games + 2) <= this.player1Games) {
                 this.player1Sets++;
                 this.resetGames();
@@ -109,22 +130,34 @@ public class Point {
                 this.resetGames();
             }
         }
-        // if tie-break
+
+        // If tie-break
         else {
-            if (this.player1Points >= 7 && (this.player2Points + 2) <= this.player1Points) {
+            // Add point to the scoring player
+            if (scoringPlayerNumber == 1) {
+                this.player1Points = String.valueOf(Integer.parseInt(this.player1Points) + 1);
+            }
+            else {
+                this.player2Points = String.valueOf(Integer.parseInt(this.player2Points) + 1);
+            }
+
+            // Check if Set is won
+            if (Integer.parseInt(this.player1Points) >= 7
+                    && (Integer.parseInt(this.player2Points) + 2) <= Integer.parseInt(this.player1Points)) {
                 this.player1Sets++;
                 this.resetPoints();
                 this.resetGames();
             }
-            else if (this.player2Points >= 7 && (this.player1Points + 2) <= this.player2Points) {
+            else if (Integer.parseInt(this.player2Points) >= 7
+                    && (Integer.parseInt(this.player1Points) + 2) <= Integer.parseInt(this.player2Points)) {
                 this.player2Sets++;
                 this.resetPoints();
                 this.resetGames();
             }
 
-            if ((this.player1Points == 0 && this.player2Points == 0)
-                    || (this.player1Points + this.player2Points) % 2 != 0
-            ) {
+            // Change player to serve on every odd point
+            if ((this.player1Points.equals("0") && this.player2Points.equals("0"))
+                    || (Integer.parseInt(this.player1Points) + Integer.parseInt(this.player2Points)) % 2 != 0) {
                 this.changePlayerToServe();
             }
         }
@@ -132,8 +165,8 @@ public class Point {
 
 
     private void resetPoints() {
-        this.player1Points = 0;
-        this.player2Points = 0;
+        this.player1Points = "0";
+        this.player2Points = "0";
     }
 
     private void resetGames() {
