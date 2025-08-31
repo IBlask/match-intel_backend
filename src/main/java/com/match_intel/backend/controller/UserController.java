8d046f45de33238b3438fa299df83c81b310286b
IBlask <95757169+IBlask@users.noreply.github.com>
2025-08-31 20:11:37 +0200

    Fix get-user-details endpoint

--- src/main/java/com/match_intel/backend/controller/UserController.java
+++ src/main/java/com/match_intel/backend/controller/UserController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -49,7 +46,7 @@ public class UserController {
     @GetMapping("/{username}")
     public ResponseEntity<UserDto> getUserByUsername(
             @AuthenticationPrincipal UserDetails currentUser,
-            @RequestParam String username
+            @PathVariable String username
     ) {
         String currentUsername = currentUser.getUsername();
         UserDto userDto = userService.getUserByUsername(currentUsername, username);
